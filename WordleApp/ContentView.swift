dd22845738bb686a10066597d3936af8035e748d
si-jaideep-singh <jaideep.singh@sportzinteractive.net>
2024-06-12 18:40:22 +0530

    Project added

---
+++ WordleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordleApp
+//
+//  Created by Jaideep Singh on 12/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Text("Jaideep")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordleApp/WordleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleAppApp.swift
+//  WordleApp
+//
+//  Created by Jaideep Singh on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
