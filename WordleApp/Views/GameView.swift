486723967acd7885762f5dc39ca189b345494ea2
si-jaideep-singh <jaideep.singh@sportzinteractive.net>
2024-06-13 11:50:25 +0530

    no message

---
+++ WordleApp/Views/GameView.swift
@@ -0,0 +1,18 @@
+//
+//  GameView.swift
+//  WordleApp
+//
+//  Created by Jaideep Singh on 13/06/24.
+//
+
+import SwiftUI
+
+struct GameView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameView()
+}
